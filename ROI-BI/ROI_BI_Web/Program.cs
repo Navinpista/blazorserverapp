2bf22d92dd8f460cc37b2bbc59b705e46f327222
Naveenkumar <Naveenkumar>
2022-09-20 20:39:35 +0530

    Publish Single exe file

--- ROI-BI/ROI_BI_Web/Program.cs
+++ ROI-BI/ROI_BI_Web/Program.cs
@@ -5,6 +5,10 @@ using Microsoft.EntityFrameworkCore;
 using ROI_BI_Lib.Data;
 using ROI_BI_Lib.Helpers;
 using MudBlazor.Services;
+using Microsoft.Extensions.Hosting;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+using System;
 
 var builder = WebApplication.CreateBuilder(args);
 builder.WebHost.UseWebRoot("wwwroot").UseStaticWebAssets();
@@ -20,6 +24,8 @@ builder.Services.AddScoped<ROIReportService>();
 builder.Services.AddScoped<ROILoginService>();
 builder.Services.AddScoped<RoiDto>();
 builder.Services.AddMudServices();
+
+builder.WebHost.UseKestrel(x => x.ListenAnyIP(6969));
 
 var Configuration = builder.Configuration;//.GetSection("ConnectionStrings");
 
@@ -44,4 +50,27 @@ app.UseRouting();
 app.MapBlazorHub();
 app.MapFallbackToPage("/_Host");
 
+//app.Start();
+
+
+if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+{
+    Process.Start(new ProcessStartInfo("http://localhost:6969/") { UseShellExecute = true });
+}
+else if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux))
+{
+    Process.Start("xdg-open", "http://localhost:6969/");
+}
+else if (RuntimeInformation.IsOSPlatform(OSPlatform.OSX))
+{
+    Process.Start("open", "http://localhost:6969/");
+}
+else
+{
+    // throw 
+}
+//OpenBrowser("http://localhost:6969/");
+
 app.Run();
+
+
